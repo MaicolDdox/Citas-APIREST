1bb20488dee8b9dc44371adae27e659065300dca
MaicolDdox <maicolduvangascarodas@gmail.com>
2026-01-10 19:19:11 -0500

    Implementacion de Rutas con Orion

---
+++ app/Http/Controllers/Api/AppointmentController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use Orion\Http\Controllers\Controller;
+use App\Models\Appointment;
+use Orion\Http\Requests\Request;
+
+
+class AppointmentController extends Controller
+{
+    protected $model = Appointment::class;
+
+
+}
--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,14 @@
 <?php
 
+use App\Http\Controllers\Api\AppointmentController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use Orion\Facades\Orion;
 
-Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::middleware('auth:sanctum')->group(function () {
+    Orion::resource('appointments', AppointmentController::class);
 });
